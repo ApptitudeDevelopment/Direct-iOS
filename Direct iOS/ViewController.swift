7c97111aac645077bf5b26329255c215ed97414d
Victor Shijie Wu <vic_wu1@hotmail.com>
2020-05-22 11:27:29 -0400

    Initial Xcode Swift Project Creation

---
+++ Direct iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Direct iOS
+//
+//  Created by Victor Shijie Wu on 2020-05-22.
+//  Copyright © 2020 Victor Shijie Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
